5750e602e04b8ea909c4907d4cbcb871b872ac66
Marhatta <vishal.marhatta@gmail.com>
2021-05-24 23:41:28 +0530

    Fixed pincode color

--- src/components/common/Input/Input.styles.js
+++ src/components/common/Input/Input.styles.js
@@ -4,4 +4,5 @@ import {Input} from 'native-base';
 export const CustomInput = styled(Input)`
   border-bottom-width: 0.5px;
   border-bottom-color: ${props => props.theme.colors.text.primary};
+  color: ${props => props.theme.colors.text.secondary};
 `;
